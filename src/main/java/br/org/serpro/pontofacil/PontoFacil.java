942b638a0398e05fb4f3d11d0196540a2e8ae5a5
Alexandre Haguiar <alexandrehaguiar@gmail.com>
2011-08-26 18:26:17 -0300

    Some changes

--- src/main/java/br/org/serpro/pontofacil/PontoFacil.java
+++ src/main/java/br/org/serpro/pontofacil/PontoFacil.java
@@ -1,32 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.org.serpro.pontofacil;
 
 import br.org.serpro.pontofacil.view.TrayIcon;
 
-/**
- *
- * @author 72767863587
- */
 public class PontoFacil {
 
-    /**
-     * Intervalor em minutos entre cada teste
-     */
     private static int intervalo = 30;
 
     public static void main(String args[]) throws InterruptedException {
-
-        TrayIcon trayIcon = new TrayIcon();
 
         SwingWorker worker = new SwingWorker() {
 
             public Object construct() {
                 while (1 != 0) {
-                    System.out.println("Ola Mundo!");
-                    Thread.sleep(10000);
+                    SiscopRN siscop = new SiscopRN();
+                    siscop.doLogin("aaa", "bbb");
+                    Relogio relogio = siscop.obterPonto();
+                    testaHorario(Relogio);
+
+                    TrayIcon trayIcon = new TrayIcon();
+                    
+                    Thread.sleep(1000 * PontoFacil.intervalo);                    
                 }
             }
         };
